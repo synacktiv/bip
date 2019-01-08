55d9e76603f421ad283ce11fe3b7de0be7a13ded
bruno <bruno>
2019-01-08 15:41:38 -0600

    update plugin hexrays_sync: split the com. sync and the struct sync, update the script for working with the current implem

---
+++ scripts/hexrays_sync_comm.py
@@ -0,0 +1,28 @@
+from bip.base import Struct
+from bip.base.utils import *
+from bip.hexrays import HexRaysEvent
+from ida_hexrays import *
+from idaapi import ctree_visitor_t, CV_FAST
+from idc import *
+from idc import GetCommentEx
+
+def hexrays_propagate_comments(event, *args):
+    if event == HexRaysEvent.hxe_func_printed:
+        f = args[0]
+        cmts = f.user_cmts
+
+        for c in cmts:
+            ida_cmt = GetCommentEx(c.ea, 0)
+            if not ida_cmt or ida_cmt.startswith('HR: '):
+                MakeComm(c.ea, 'HR: '+f.get_user_cmt(c, 1))
+
+    return 0
+
+
+def install():
+    install_hexrays_callback(hexrays_propagate_comments)
+
+def uninstall():
+    remove_hexrays_callback(hexrays_propagate_comments)
+
+
