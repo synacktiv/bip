4cbb74f29fd488ac7b74c78d6838ba75199d7016
bruno <bruno>
2019-01-17 10:46:27 -0600

    add examples for patching ida function call

---
+++ examples/patch_call_func.py
@@ -0,0 +1,22 @@
+
+
+name = "hello"
+sc = '\xff\xd0' # call rax
+
+# name is the name of the function,
+# sc is a string of bytes
+# This does not remove the xref to the IdaFunction 
+def patch_call_to_func(name, sc):
+    f = IdaFunction.get_by_name(name)
+    for i in f.xCodeTo:
+        if i.mnem == "call":
+            if i.size < len(sc):
+                print("Could not patch {} (not enough place)".format(i))
+                continue
+            s = sc + ("\x90" * (i.size - len(sc)))
+            i.bytes = s
+        else:
+            print("Not a call at {}".format(i))
+
+
+
