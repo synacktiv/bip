309eaefddf90ff6665b725cb3953214ae4a52f25
bruno <bruno>
2019-02-10 15:59:00 -0600

    cleanning implementation comment, changing statement class name and adding basic printing for HxCItem object

--- bip/hexrays/hx_citem.py
+++ bip/hexrays/hx_citem.py
@@ -6,7 +6,7 @@ class HxCType(object):
         HexRays. This is a wrapper on top of the ``ctype_t`` enum: ``cot_*``
         are for the expresion (``cexpr_t`` in ida, :class:`HxCExpr` in bip )
         and ``cit_*`` are for the statement (``cinsn_t`` in ida,
-        :class:`HxCStatement` in bip). This also include some static function
+        :class:`HxCStmt` in bip). This also include some static function
         which are wrapper which manipulate those types.
 
         .. todo:: static function for manipulating the enum ?
@@ -16,75 +16,75 @@ class HxCType(object):
         Comment on the enum are from ``hexrays.hpp`` .
     """
     COT_EMPTY       = 0
-    COT_COMMA       = 1     #: x, y                             # DONE
-    COT_ASG         = 2     #: x = y                            # DONE
-    COT_ASGBOR      = 3     #: x |= y                       # DONE
-    COT_ASGXOR      = 4     #: x ^= y                       # DONE
-    COT_ASGBAND     = 5     #: x &= y                       # DONE
-    COT_ASGADD      = 6     #: x += y                       # DONE
-    COT_ASGSUB      = 7     #: x -= y                       # DONE
-    COT_ASGMUL      = 8     #: x *= y                       # DONE
-    COT_ASGSSHR     = 9     #: x >>= y signed               # DONE
-    COT_ASGUSHR     = 10    #: x >>= y unsigned             # DONE
-    COT_ASGSHL      = 11    #: x <<= y                      # DONE
-    COT_ASGSDIV     = 12    #: x /= y signed                # DONE
-    COT_ASGUDIV     = 13    #: x /= y unsigned              # DONE
-    COT_ASGSMOD     = 14    #: x %= y signed                # DONE
-    COT_ASGUMOD     = 15    #: x %= y unsigned              # DONE
-    COT_TERN        = 16    #: x ? y : z                    # DONE
-    COT_LOR         = 17    #: x || y                       # DONE
-    COT_LAND        = 18    #: x && y                       # DONE
-    COT_BOR         = 19    #: x | y                        # DONE
-    COT_XOR         = 20    #: x ^ y                        # DONE
-    COT_BAND        = 21    #: x & y                        # DONE
-    COT_EQ          = 22    #: x == y int or fpu (see EXFL_FPOP)             # DONE
-    COT_NE          = 23    #: x != y int or fpu (see EXFL_FPOP)             # DONE
-    COT_SGE         = 24    #: x >= y signed or fpu (see EXFL_FPOP)             # DONE
-    COT_UGE         = 25    #: x >= y unsigned                                  # DONE
-    COT_SLE         = 26    #: x <= y signed or fpu (see EXFL_FPOP)             # DONE
-    COT_ULE         = 27    #: x <= y unsigned                                  # DONE
-    COT_SGT         = 28    #: x >  y signed or fpu (see EXFL_FPOP)             # DONE
-    COT_UGT         = 29    #: x >  y unsigned                                  # DONE
-    COT_SLT         = 30    #: x <  y signed or fpu (see EXFL_FPOP)             # DONE
-    COT_ULT         = 31    #: x <  y unsigned                                  # DONE
-    COT_SSHR        = 32    #: x >> y signed                                    # DONE
-    COT_USHR        = 33    #: x >> y unsigned                                  # DONE
-    COT_SHL         = 34    #: x << y                                           # DONE
-    COT_ADD         = 35    #: x + y                                            # DONE
-    COT_SUB         = 36    #: x - y                                            # DONE
-    COT_MUL         = 37    #: x * y                                            # DONE
-    COT_SDIV        = 38    #: x / y signed                 # DONE
-    COT_UDIV        = 39    #: x / y unsigned               # DONE
-    COT_SMOD        = 40    #: x % y signed                 # DONE
-    COT_UMOD        = 41    #: x % y unsigned               # DONE
-    COT_FADD        = 42    #: x + y fp                     # DONE
-    COT_FSUB        = 43    #: x - y fp                     # DONE
-    COT_FMUL        = 44    #: x * y fp                     # DONE
-    COT_FDIV        = 45    #: x / y fp                     # DONE
-    COT_FNEG        = 46    #: -x fp                        # DONE
-    COT_NEG         = 47    #: -x                           # DONE
-    COT_CAST        = 48    #: (type)x                      # DONE
-    COT_LNOT        = 49    #: !x                           # DONE
-    COT_BNOT        = 50    #: ~x                           # DONE
-    COT_PTR         = 51    #: *x, access size in 'ptrsize' # DONE
-    COT_REF         = 52    #: &x                           # DONE
-    COT_POSTINC     = 53    #: x++                          # DONE
-    COT_POSTDEC     = 54    #: x--                          # DONE
-    COT_PREINC      = 55    #: ++x                          # DONE
-    COT_PREDEC      = 56    #: --x                          # DONE
-    COT_CALL        = 57    #: x(...)                       # DONE
-    COT_IDX         = 58    #: x[y]                         # DONE
-    COT_MEMREF      = 59    #: x.m                          # DONE
-    COT_MEMPTR      = 60    #: x->m, access size in 'ptrsize'   # DONE
-    COT_NUM         = 61    #: n                                # DONE # TODO mother class
-    COT_FNUM        = 62    #: fpc                              # DONE # TODO mother class
-    COT_STR         = 63    #: string constant                  # DONE # TODO mother class
-    COT_OBJ         = 64    #: obj_ea                           # DONE # TODO mother class
-    COT_VAR         = 65    #: v                                # DONE # TODO mother class
-    COT_INSN        = 66    #: instruction in expression, internal representation only # DONE
-    COT_SIZEOF      = 67    #: sizeof(x)                # DONE
-    COT_HELPER      = 68    #: arbitrary name           # DONE
-    COT_TYPE        = 69    #: arbitrary type           # DONE
+    COT_COMMA       = 1     #: x, y
+    COT_ASG         = 2     #: x = y
+    COT_ASGBOR      = 3     #: x |= y
+    COT_ASGXOR      = 4     #: x ^= y
+    COT_ASGBAND     = 5     #: x &= y
+    COT_ASGADD      = 6     #: x += y
+    COT_ASGSUB      = 7     #: x -= y
+    COT_ASGMUL      = 8     #: x *= y
+    COT_ASGSSHR     = 9     #: x >>= y signed
+    COT_ASGUSHR     = 10    #: x >>= y unsigned
+    COT_ASGSHL      = 11    #: x <<= y
+    COT_ASGSDIV     = 12    #: x /= y signed
+    COT_ASGUDIV     = 13    #: x /= y unsigned
+    COT_ASGSMOD     = 14    #: x %= y signed
+    COT_ASGUMOD     = 15    #: x %= y unsigned
+    COT_TERN        = 16    #: x ? y : z
+    COT_LOR         = 17    #: x || y
+    COT_LAND        = 18    #: x && y
+    COT_BOR         = 19    #: x | y
+    COT_XOR         = 20    #: x ^ y
+    COT_BAND        = 21    #: x & y
+    COT_EQ          = 22    #: x == y int or fpu (see EXFL_FPOP)
+    COT_NE          = 23    #: x != y int or fpu (see EXFL_FPOP)
+    COT_SGE         = 24    #: x >= y signed or fpu (see EXFL_FPOP)
+    COT_UGE         = 25    #: x >= y unsigned
+    COT_SLE         = 26    #: x <= y signed or fpu (see EXFL_FPOP)
+    COT_ULE         = 27    #: x <= y unsigned
+    COT_SGT         = 28    #: x >  y signed or fpu (see EXFL_FPOP)
+    COT_UGT         = 29    #: x >  y unsigned
+    COT_SLT         = 30    #: x <  y signed or fpu (see EXFL_FPOP)
+    COT_ULT         = 31    #: x <  y unsigned
+    COT_SSHR        = 32    #: x >> y signed
+    COT_USHR        = 33    #: x >> y unsigned
+    COT_SHL         = 34    #: x << y
+    COT_ADD         = 35    #: x + y
+    COT_SUB         = 36    #: x - y
+    COT_MUL         = 37    #: x * y
+    COT_SDIV        = 38    #: x / y signed
+    COT_UDIV        = 39    #: x / y unsigned
+    COT_SMOD        = 40    #: x % y signed
+    COT_UMOD        = 41    #: x % y unsigned
+    COT_FADD        = 42    #: x + y fp
+    COT_FSUB        = 43    #: x - y fp
+    COT_FMUL        = 44    #: x * y fp
+    COT_FDIV        = 45    #: x / y fp
+    COT_FNEG        = 46    #: -x fp
+    COT_NEG         = 47    #: -x
+    COT_CAST        = 48    #: (type)x
+    COT_LNOT        = 49    #: !x
+    COT_BNOT        = 50    #: ~x
+    COT_PTR         = 51    #: *x, access size in 'ptrsize'
+    COT_REF         = 52    #: &x
+    COT_POSTINC     = 53    #: x++
+    COT_POSTDEC     = 54    #: x--
+    COT_PREINC      = 55    #: ++x
+    COT_PREDEC      = 56    #: --x
+    COT_CALL        = 57    #: x(...)
+    COT_IDX         = 58    #: x[y]
+    COT_MEMREF      = 59    #: x.m
+    COT_MEMPTR      = 60    #: x->m, access size in 'ptrsize'
+    COT_NUM         = 61    #: n
+    COT_FNUM        = 62    #: fpc
+    COT_STR         = 63    #: string constant
+    COT_OBJ         = 64    #: obj_ea
+    COT_VAR         = 65    #: v
+    COT_INSN        = 66    #: instruction in expression, internal representation only
+    COT_SIZEOF      = 67    #: sizeof(x)
+    COT_HELPER      = 68    #: arbitrary name
+    COT_TYPE        = 69    #: arbitrary type
     COT_LAST        = 69    #: All before this are ``cexpr_t`` after are ``cinsn_t``
     CIT_EMPTY       = 70    #: instruction types start here
     CIT_BLOCK       = 71    #: block-statement: { ... }
@@ -130,7 +130,7 @@ class HxCItem(object):
         """
         #: The ``citem_t`` object from ida, this is conserved at this level
         #:  for providing a few functionnality compatible between
-        #:  :class:`HxCExpr` and :class:`HxCStatement` .
+        #:  :class:`HxCExpr` and :class:`HxCStmt` .
         self._citem = citem
 
     @property
@@ -154,7 +154,7 @@ class HxCItem(object):
     def is_statement(self):
         """
             Property which return true if this item is a C Statement
-            (:class:`HxCStatement`, ``cinsn_t``).
+            (:class:`HxCStmt`, ``cinsn_t``).
         """
         return not self.is_expr
 
@@ -167,6 +167,14 @@ class HxCItem(object):
             :return int: One of the :class:`HxCType` constant value.
         """
         return self._citem.op
+
+    def __str__(self):
+        """
+            Convert a citem to a string.
+
+            This is surcharge both by :class:`HxCStmt` and :class:`HxCExpr`.
+        """
+        return "{}(ea=0x{:X})".format(self.__class__.__name__, self.ea)
 
     @classmethod
     def is_handling_type(cls, typ):
