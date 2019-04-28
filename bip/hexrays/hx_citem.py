c6ca24c2320ab86f32e192971c76694f9b9ec540
bruno <bruno>
2019-04-28 03:04:38 -0500

    bip/hexrays: update comment for HxCType

--- bip/hexrays/hx_citem.py
+++ bip/hexrays/hx_citem.py
@@ -14,73 +14,73 @@ class HxCType(object):
         Comment on the enum are from ``hexrays.hpp`` .
     """
     COT_EMPTY       = 0
-    COT_COMMA       = 1     #: x, y
-    COT_ASG         = 2     #: x = y
-    COT_ASGBOR      = 3     #: x |= y
-    COT_ASGXOR      = 4     #: x ^= y
-    COT_ASGBAND     = 5     #: x &= y
-    COT_ASGADD      = 6     #: x += y
-    COT_ASGSUB      = 7     #: x -= y
-    COT_ASGMUL      = 8     #: x *= y
-    COT_ASGSSHR     = 9     #: x >>= y signed
-    COT_ASGUSHR     = 10    #: x >>= y unsigned
-    COT_ASGSHL      = 11    #: x <<= y
-    COT_ASGSDIV     = 12    #: x /= y signed
-    COT_ASGUDIV     = 13    #: x /= y unsigned
-    COT_ASGSMOD     = 14    #: x %= y signed
-    COT_ASGUMOD     = 15    #: x %= y unsigned
-    COT_TERN        = 16    #: x ? y : z
-    COT_LOR         = 17    #: x || y
-    COT_LAND        = 18    #: x && y
-    COT_BOR         = 19    #: x | y
-    COT_XOR         = 20    #: x ^ y
-    COT_BAND        = 21    #: x & y
-    COT_EQ          = 22    #: x == y int or fpu (see EXFL_FPOP)
-    COT_NE          = 23    #: x != y int or fpu (see EXFL_FPOP)
-    COT_SGE         = 24    #: x >= y signed or fpu (see EXFL_FPOP)
-    COT_UGE         = 25    #: x >= y unsigned
-    COT_SLE         = 26    #: x <= y signed or fpu (see EXFL_FPOP)
-    COT_ULE         = 27    #: x <= y unsigned
-    COT_SGT         = 28    #: x >  y signed or fpu (see EXFL_FPOP)
-    COT_UGT         = 29    #: x >  y unsigned
-    COT_SLT         = 30    #: x <  y signed or fpu (see EXFL_FPOP)
-    COT_ULT         = 31    #: x <  y unsigned
-    COT_SSHR        = 32    #: x >> y signed
-    COT_USHR        = 33    #: x >> y unsigned
-    COT_SHL         = 34    #: x << y
-    COT_ADD         = 35    #: x + y
-    COT_SUB         = 36    #: x - y
-    COT_MUL         = 37    #: x * y
-    COT_SDIV        = 38    #: x / y signed
-    COT_UDIV        = 39    #: x / y unsigned
-    COT_SMOD        = 40    #: x % y signed
-    COT_UMOD        = 41    #: x % y unsigned
-    COT_FADD        = 42    #: x + y fp
-    COT_FSUB        = 43    #: x - y fp
-    COT_FMUL        = 44    #: x * y fp
-    COT_FDIV        = 45    #: x / y fp
-    COT_FNEG        = 46    #: -x fp
-    COT_NEG         = 47    #: -x
-    COT_CAST        = 48    #: (type)x
-    COT_LNOT        = 49    #: !x
-    COT_BNOT        = 50    #: ~x
-    COT_PTR         = 51    #: *x, access size in 'ptrsize'
-    COT_REF         = 52    #: &x
-    COT_POSTINC     = 53    #: x++
-    COT_POSTDEC     = 54    #: x--
-    COT_PREINC      = 55    #: ++x
-    COT_PREDEC      = 56    #: --x
-    COT_CALL        = 57    #: x(...)
-    COT_IDX         = 58    #: x[y]
-    COT_MEMREF      = 59    #: x.m
-    COT_MEMPTR      = 60    #: x->m, access size in 'ptrsize'
+    COT_COMMA       = 1     #: ``x, y``
+    COT_ASG         = 2     #: ``x = y``
+    COT_ASGBOR      = 3     #: ``x |= y``
+    COT_ASGXOR      = 4     #: ``x ^= y``
+    COT_ASGBAND     = 5     #: ``x &= y``
+    COT_ASGADD      = 6     #: ``x += y``
+    COT_ASGSUB      = 7     #: ``x -= y``
+    COT_ASGMUL      = 8     #: ``x *= y``
+    COT_ASGSSHR     = 9     #: ``x >>= y`` signed
+    COT_ASGUSHR     = 10    #: ``x >>= y`` unsigned
+    COT_ASGSHL      = 11    #: ``x <<= y``
+    COT_ASGSDIV     = 12    #: ``x /= y`` signed
+    COT_ASGUDIV     = 13    #: ``x /= y`` unsigned
+    COT_ASGSMOD     = 14    #: ``x %= y`` signed
+    COT_ASGUMOD     = 15    #: ``x %= y`` unsigned
+    COT_TERN        = 16    #: ``x ? y : z``
+    COT_LOR         = 17    #: ``x || y``
+    COT_LAND        = 18    #: ``x && y``
+    COT_BOR         = 19    #: ``x | y``
+    COT_XOR         = 20    #: ``x ^ y``
+    COT_BAND        = 21    #: ``x & y``
+    COT_EQ          = 22    #: ``x == y`` int or fpu (see EXFL_FPOP)
+    COT_NE          = 23    #: ``x != y`` int or fpu (see EXFL_FPOP)
+    COT_SGE         = 24    #: ``x >= y`` signed or fpu (see EXFL_FPOP)
+    COT_UGE         = 25    #: ``x >= y`` unsigned
+    COT_SLE         = 26    #: ``x <= y`` signed or fpu (see EXFL_FPOP)
+    COT_ULE         = 27    #: ``x <= y`` unsigned
+    COT_SGT         = 28    #: ``x >  y`` signed or fpu (see EXFL_FPOP)
+    COT_UGT         = 29    #: ``x >  y`` unsigned
+    COT_SLT         = 30    #: ``x <  y`` signed or fpu (see EXFL_FPOP)
+    COT_ULT         = 31    #: ``x <  y`` unsigned
+    COT_SSHR        = 32    #: ``x >> y`` signed
+    COT_USHR        = 33    #: ``x >> y`` unsigned
+    COT_SHL         = 34    #: ``x << y``
+    COT_ADD         = 35    #: ``x + y``
+    COT_SUB         = 36    #: ``x - y``
+    COT_MUL         = 37    #: ``x * y``
+    COT_SDIV        = 38    #: ``x / y`` signed
+    COT_UDIV        = 39    #: ``x / y`` unsigned
+    COT_SMOD        = 40    #: ``x % y`` signed
+    COT_UMOD        = 41    #: ``x % y`` unsigned
+    COT_FADD        = 42    #: ``x + y`` fp
+    COT_FSUB        = 43    #: ``x - y`` fp
+    COT_FMUL        = 44    #: ``x * y`` fp
+    COT_FDIV        = 45    #: ``x / y`` fp
+    COT_FNEG        = 46    #: ``-x`` fp
+    COT_NEG         = 47    #: ``-x``
+    COT_CAST        = 48    #: ``(type)x``
+    COT_LNOT        = 49    #: ``!x``
+    COT_BNOT        = 50    #: ``~x``
+    COT_PTR         = 51    #: ``*x``, access size in 'ptrsize'
+    COT_REF         = 52    #: ``&x``
+    COT_POSTINC     = 53    #: ``x++``
+    COT_POSTDEC     = 54    #: ``x--``
+    COT_PREINC      = 55    #: ``++x``
+    COT_PREDEC      = 56    #: ``--x``
+    COT_CALL        = 57    #: ``x(...)``
+    COT_IDX         = 58    #: ``x[y]``
+    COT_MEMREF      = 59    #: ``x.m``
+    COT_MEMPTR      = 60    #: ``x->m``, access size in 'ptrsize'
     COT_NUM         = 61    #: n
     COT_FNUM        = 62    #: fpc
     COT_STR         = 63    #: string constant
     COT_OBJ         = 64    #: obj_ea
     COT_VAR         = 65    #: v
     COT_INSN        = 66    #: instruction in expression, internal representation only
-    COT_SIZEOF      = 67    #: sizeof(x)
+    COT_SIZEOF      = 67    #: ``sizeof(x)``
     COT_HELPER      = 68    #: arbitrary name
     COT_TYPE        = 69    #: arbitrary type
     COT_LAST        = 69    #: All before this are ``cexpr_t`` after are ``cinsn_t``
