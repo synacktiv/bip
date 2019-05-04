86fbabbf10964def4ade9f204ea543dd0923bfb7
bruno <bruno>
2019-05-04 07:01:40 -0500

    bip/base: fix include for the type

--- bip/base/__init__.py
+++ bip/base/__init__.py
@@ -7,4 +7,4 @@ from xref import XrefTypes, IdaXref
 from biperror import BipError
 from func import IdaFuncFlags, IdaFunction
 from block import IdaBlockType, IdaBlock
-from .type import IdaType, ITypeVoid, ITypeInt, ITypeBool, ITypeFloat, ITypePtr, ITypeArray, ITypeFunc, ITypeStruct, ITypeUnion, ITypeEnum
+from .type import IdaType, ITypePartial, ITypeVoid, ITypeInt, ITypeBool, ITypeFloat, ITypePtr, ITypeArray, ITypeFunc, ITypeStruct, ITypeUnion, ITypeEnum
