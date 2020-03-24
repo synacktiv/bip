6450c04e655ada93c8ab6c36a7f985797fd1d1c6
bruno <bruno>
2020-03-24 16:05:33 +0100

    test: add test for manipulating the enum members

--- test/test_enum.py
+++ test/test_enum.py
@@ -19,6 +19,7 @@ def test_bipenum1():
     assert isinstance(be, BipEnum)
     # equality
     assert be == be2
+    assert (be != be2) == False
     assert be == be._eid
     assert be != 0
     assert be != idc.BADADDR
@@ -74,11 +75,70 @@ def test_bipenum2():
     BipEnum.delete(be._eid)
     with pytest.raises(ValueError):
         BipEnum.get("testenum3")
-        
 
+def test_bipenum3():
+    # creating, accessing and deleting members
+    be = BipEnum.create("testenum")
+    assert be.nb_members == 0
+    be.add("testmem", 0x10)
+    assert be.nb_members == 1
+    bem = be.member_by_name("testmem")
+    assert bem is not None
+    assert isinstance(bem, BEnumMember)
+    assert bem.name == "testmem"
+    assert bem.value == 0x10
+    assert bem == be["testmem"]
+    beml = be.members_by_value(0x10)
+    assert len(beml) == 1
+    assert beml[0] == bem
+    with pytest.raises(RuntimeError):
+        be.add("testmem", 0x11)
+    be.add("testmem2", 0x10)
+    beml = be.members_by_value(0x10)
+    assert len(beml) == 2
+    assert beml[0] != beml[1]
+    be.add("testmem3", 0x10)
+    be.add("testmem4", 0x1)
+    assert be.nb_members == 4
+    be.del_member("testmem2")
+    assert be.nb_members == 3
+    with pytest.raises(ValueError):
+        be.del_member("testmem2")
+    beml = be.members_by_value(0x10)
+    assert len(beml) == 2
+    assert beml[0] != beml[1]
+    assert "testmem3" in [m.name for m in beml]
+    assert "testmem4" not in [m.name for m in beml]
+    assert len(be.members) == 3
+    for m in be:
+        str(m)
+    BipEnum.delete("testenum")
 
-
-
+def test_bipenum4():
+    # members
+    be = BipEnum.create("testenum")
+    assert be.nb_members == 0
+    be.add("testmem", 0x10)
+    assert be.nb_members == 1
+    bem = be.member_by_name("testmem")
+    bem2 = BEnumMember.get("testmem")
+    assert bem == bem2
+    assert bem.name == "testmem"
+    assert bem.value == 0x10
+    bem.name = "newname"
+    assert bem.name == "newname"
+    assert bem2.name == "newname"
+    assert bem2.enum == be
+    assert bem2.value == 0x10
+    bem.name = "testmem"
+    assert bem.name == "testmem"
+    assert bem.comment is None
+    assert bem.rcomment is None
+    bem.comment = "hello"
+    bem.rcomment = "test rcom"
+    assert bem.comment == "hello"
+    assert bem.rcomment == "test rcom"
+    BipEnum.delete("testenum")
 
 
 
