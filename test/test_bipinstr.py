d52f2d17a7a94a9f4870736e350662b7d48177bc
bruno <bruno>
2020-04-15 21:42:26 +0200

    test: finalize test for bip instruction

--- test/test_bipinstr.py
+++ test/test_bipinstr.py
@@ -48,6 +48,30 @@ def test_bipinstr02():
     assert Instr(0x01800D327F).is_call == False
     assert Instr(0x01800D3011).is_call == True
     assert Instr(0x01800D31E8).is_call == False
+
+    assert Instr(0x01800D324E).is_ret == False
+    assert Instr(0x01800D3269).is_ret == True
+
+    assert Instr(0x01800D3269).is_indirect_jmp == False
+    assert Instr(0x01800D324E).is_indirect_jmp == False
+    assert Instr(0x01800D323A).is_indirect_jmp == False
+    assert Instr(0x01800D3240).is_indirect_jmp == False
+    assert Instr(0x01800FF5D7).is_indirect_jmp == True
+
+    assert Instr(0x01800D3269).is_end_block == True
+    assert Instr(0x01800D324E).is_end_block == True
+    assert Instr(0x01800D324B).is_end_block == False
+    assert Instr(0x01800D3240).is_end_block == True
+    assert Instr(0x01800D3253).is_end_block == True
+    assert Instr(0x01800D322E).is_end_block == False
+
+    assert Instr(0x01800D3269).is_end_block_call == True
+    assert Instr(0x01800D324E).is_end_block_call == True
+    assert Instr(0x01800D324B).is_end_block_call == False
+    assert Instr(0x01800D3240).is_end_block_call == True
+    assert Instr(0x01800D3253).is_end_block_call == True
+    assert Instr(0x01800D322E).is_end_block_call == True
+
     assert Instr(0x01800D327F).is_in_func == True
     # TODO: add test for Instruction not in func (other binary)
 
@@ -58,36 +82,34 @@ def test_bipinstr03():
     assert Instr(0x01800D3269).next.ea == 0X01800D326A
     assert Instr(0x01800D327F).next is None
 
-def test_instr9():
+def test_bipinstr04():
+    # func and block
+    assert Instr(0x01800D6B33).func.ea == 0x01800D6B30
+    assert Instr(0x01800D6B33).func.__class__ == BipFunction
+    assert Instr(0x01800D323A).block.ea == 0x01800D3227
+    assert Instr(0x01800D323A).block.__class__ == BipBlock
+    # TODO: add test for instr not in func or block
+
+def test_bipinstr05():
+    # control flow
     assert Instr(0x01800D325A).xOrdinaryCfNext.ea == 0x01800D325E
-
-def test_instrA():
     assert [x.ea for x in Instr(0x01800D325A).xCfNext] == [0x01800D325E]
-
-def test_instrB():
     assert [x.ea for x in Instr(0x01800D325A).xCfPrev] == [0x01800D3253, 0x01800D3028]
-
-def test_instrC():
     assert Instr(0x01800D3028).xOrdinaryCfNext is None
-
-def test_instrD():
     assert [x.ea for x in Instr(0x01800D3028).xCfNext] == [0x01800D325A]
-
-def test_instrE():
     assert [x.ea for x in Instr(0x01800D3028).xCfPrev] == [0x01800D3023]
-
-def test_instrF():
     assert [x.ea for x in Instr(0x01800D324E).xCfNext] == [0x01800D3253, 0x01800D35E8]
-
-def test_instr10():
     assert [x.ea for x in Instr(0x01800D323A).xCfNext] == [0x01800D323C, 0x01800D3242]
 
-def test_instr11():
-    assert Instr(0x01800D6B33).func.ea == 0x01800D6B30
-
-def test_instr12():
-    assert Instr(0x01800D323A).block.ea == 0x01800D3227
-
+def test_bipinstr06():
+    # cmp and hash
+    assert BipElt(0x01800D325A) == Instr(0x01800D325A)
+    assert Instr(0x01800D325A) == Instr(0x01800D325A)
+    assert Instr(0x01800D325A) > Instr(0x01800D3028)
+    assert Instr(0x01800D3028) < Instr(0x01800D325A)
+    assert len([Instr(0x01800D325A), Instr(0x01800D325A)]) == 2
+    assert len(set([Instr(0x01800D325A), Instr(0x01800D325A)])) == 1
+    assert len(set([BipElt(0x01800D325A), Instr(0x01800D325A)])) == 2
 
 
 
