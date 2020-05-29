df91e57019a401f5397a26b62904cb1ddb44934a
bruno <bruno>
2020-05-29 12:23:06 +0200

    test: add test for bip function

--- test/test_bipfunc.py
+++ test/test_bipfunc.py
@@ -4,6 +4,8 @@ sys.path.append(r"E:\bip")
 from bip.base import *
 from bip.hexrays import HxCFunc
 
+import ida_typeinf
+
 import pytest
 
 """
@@ -178,10 +180,59 @@ def test_bipfunc06():
     assert isinstance(BipFunction(0x01800D324E).bytes, list) == True
     assert isinstance(BipFunction(0x01800D324E).bytes[0], int) == True
 
-def test_funcB():
-    assert BipFunction(0x01800D6B30).guess_strtype == "__int64 __fastcall()"
+def test_bipfunc07():
+    # type
+    assert BipFunction(0x1800D2FF0).str_type is None
+    assert BipFunction(0x1800D2FF0).guess_strtype == '__int64 __fastcall()'
+    BipFunction(0x1800D2FF0).str_type = "void *func(int a)"
+    assert BipFunction(0x1800D2FF0).str_type == 'void *__stdcall(int a)'
+    BipFunction(0x1800D2FF0).str_type = ""
+    assert BipFunction(0x1800D2FF0).str_type is None
+    assert isinstance(BipFunction(0x1800D2FF0)._ida_tinfo, ida_typeinf.tinfo_t)
 
-# TODO finish test from TYPE, ARGS, ... (~l750)
-# remove the todo test in bip/base/func.py
+def test_bipfunc08():
+    # xref, (j)callers and callees
+    assert len(BipFunction(0x1800D2FF0).xTo) == 0x4
+    assert isinstance(BipFunction(0x1800D2FF0).xTo[0], BipXref)
+    assert len(BipFunction(0x1800D2FF0).xEaTo) == 0x4
+    assert isinstance(BipFunction(0x1800D2FF0).xEaTo[0], (int, long)) == True
+    assert isinstance(BipFunction(0x1800D2FF0).xEltTo[0], BipElt) == True
+    assert len(BipFunction(0x1800D2FF0).xEltTo) == 0x4
+    assert isinstance(BipFunction(0x1800D2FF0).xCodeTo[0], Instr) == True
+    assert len(BipFunction(0x1800D2FF0).xCodeTo) == 0x1
+    assert len(BipFunction(0x1800D2FF0).callers) == 0x1
+    assert isinstance(BipFunction(0x1800D2FF0).callers[0], BipFunction) == True
+    assert len(BipFunction(0x1800D2FF0).jcallers) == 0x1
+    assert isinstance(BipFunction(0x1800D2FF0).jcallers[0], BipFunction) == True
+    assert len(BipFunction(0x0180009DC0).callers) == 0x31
+    assert len(BipFunction(0x0180009DC0).jcallers) == 0x34
+
+
+def test_bipfunc09():
+    # class methods
+    assert isinstance(BipFunction.ByOrdinal(0), BipFunction)
+    assert (BipFunction.ByOrdinal(0) ==  BipFunction(0x180001010))
+    assert (BipFunction.iter_all().next() ==  BipFunction(0x180001010))
+    assert len([f for f in BipFunction.iter_all()]) == 0xecd
+    assert len(BipFunction.Entries()) == 0x926
+    assert isinstance(BipFunction.Entries()[0], BipFunction)
+    assert BipFunction.Entries()[1] == BipFunction(0x18003D190)
+    assert len([e for e in BipFunction.Entries_iter()]) == 0x926
+    assert isinstance(BipFunction.Entries_iter().next(), BipFunction)
+    assert BipFunction.Entries_iter().next() == BipFunction(0x018007D0C0)
+    assert (BipFunction.get_by_name("RtlFindClearBits") ==  BipFunction(0x180001010))
+    assert (BipFunction.get_by_name("DonotExist") is None)
+    assert (BipFunction.get_by_name("") is None)
+    assert (BipFunction.get_by_name(None) is None)
+    assert len(BipFunction.get_by_prefix("Rtl")) == 0x382
+    assert (BipFunction.get_by_prefix("Rtl")[0] == BipFunction(0x180001010))
+    assert len(BipFunction.get_by_regex("^Rtl.*Bits$")) == 0x8
+    assert BipFunction.get_by_regex("^Rtl.*Bits$")[0] == BipFunction(0x180001010)
+    # TODO: make test for create, need other binary ?
+
+def test_bibfunc0A():
+    # static method
+    assert BipFunction.Count() == 0xecd
+
 
 
