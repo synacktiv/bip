4afdc5111814cd230b109e49e5fd5b38634ef670
bruno <bruno>
2020-06-01 14:22:44 +0200

    test: add test for BipBlock class and remove useless test_basic

--- test/launch_test.py
+++ test/launch_test.py
@@ -2,8 +2,9 @@ import pytest
 
 pytest.main([r"test_bipelt.py",  "--capture=sys"])
 pytest.main([r"test_bipinstr.py",  "--capture=sys"])
-pytest.main([r"test_basic.py",  "--capture=sys"])
 pytest.main([r"test_enum.py",  "--capture=sys"])
 pytest.main([r"test_bipfunc.py",  "--capture=sys"])
 pytest.main([r"test_bipstruct.py",  "--capture=sys"])
 pytest.main([r"test_hx_base.py",  "--capture=sys"])
+pytest.main([r"test_bipblock.py",  "--capture=sys"])
+
--- test/test_bipblock.py
+++ test/test_bipblock.py
@@ -1,82 +1,52 @@
-import sys
-
 from bip.base import *
-
-import idc
-import ida_bytes
 
 import pytest
 
-# TODO:
-#   * Operand
-#   * xref
-#   * struct
-#   * utils
+"""
+Test for class :class:`BipBlock` in ``bip/base/bipblock.py``.
+"""
 
-###################### BLOCK #########################
 
-def test_block0():
+
+def test_bipblock00():
+    # base
     assert BipBlock(0x0180099990).ea == BipFunction(0x0180099990).ea
-
-def test_block1():
     assert BipBlock(0x0180099990).ea == BipBlock(0x0180099992).ea
+    assert BipBlock(0x0180099990).end == 0x01800999DC
+    with pytest.raises(TypeError): BipBlock("AString")
+    with pytest.raises(ValueError): BipBlock(0x10) # invalid addr
+    assert str(BipBlock(0x0180099990)) == 'BipBlock: 0x180099990 (from Func: RtlRaiseStatus (0x180099990))'
+    assert isinstance(BipFunction(0x0180099990).blocks[0], BipBlock) == True
+    assert BipFunction(0x0180099990).blocks[0].ea == 0x180099990
 
-def test_block2():
+def test_bipblock01():
+    # Type & info
     assert BipBlock(0x0180099990).type == BipBlockType.FCB_NORMAL
-
-def test_block3():
     assert BipBlock(0x0180099990).is_ret == False
-
-def test_block4():
     assert BipBlock(0x0180099990).is_noret == False
-
-def test_block5():
     assert BipBlock(0x0180099990).is_external == False
-
-def test_block6():
     assert BipBlock(0x01800999F0).type == BipBlockType.FCB_NORET
-
-def test_block7():
     assert BipBlock(0x01800999F0).is_ret == False
-
-def test_block8():
     assert BipBlock(0x01800999F0).is_noret == True
-
-def test_block9():
     assert BipBlock(0x01800999F0).is_external == False
 
-def test_blockA():
-    assert BipBlock(0x0180099990).end == 0x01800999DC
-
-def test_blockB():
+def test_bipblock02():
+    # control flow
     assert len(BipBlock(0x0180099990).succ) == 2
-
-def test_blockC():
     ss = BipBlock(0x0180099990).succ
+    assert isinstance(ss[0], BipBlock)
     assert ss[0].ea == 0x01800999DC
     assert ss[1].ea == 0x01800999F0
-
-def test_blockD():
     b = BipBlock(0x0180099990)
     ss = b.succ
     i = 0
     for bb in b.succ_iter:
         assert ss[i].ea == bb.ea
         i += 1
-
-def test_blockE():
     assert len(BipBlock(0x01800999E4).succ) == 1
-
-def test_blockF():
     assert len(BipBlock(0x01800999F0).succ) == 0
-
-def test_block10():
     assert len(BipBlock(0x01800999F0).pred) == 2
-
-def test_block11():
     assert len(BipBlock(0x0180099990).pred) == 0
-
-def test_block12():
     b = BipBlock(0x01800999F0)
     ss = b.pred
     i = 0
@@ -84,27 +54,36 @@ def test_block12():
         assert ss[i].ea == bb.ea
         i += 1
 
-def test_block13():
+def test_bipblock03():
+    # func, instr, items, bytes
+    assert isinstance(BipBlock(0x01800999F0).func, BipFunction)
     assert BipBlock(0x01800999F0).func.ea == 0x0180099990
-
-def test_block14():
     assert len(BipBlock(0x01800999DC).items) == 4
-
-def test_block15():
     for i in BipBlock(0x01800999DC).items:
         assert i.__class__ == Instr
-
-def test_block16():
     assert len(BipBlock(0x01800999DC).instr) == 4
-
-def test_block17():
     for i in BipBlock(0x01800999DC).instr:
         assert i.__class__ == Instr
-
-def test_block18():
     assert BipBlock(0x01800999DC).instr[-1].ea == 0x01800999EE
-
-def test_block19():
     assert BipBlock(0x01800999DC).bytes == [0x48, 0x8D, 0x94, 0x24, 0xC0, 0x00, 0x00, 0x00, 0x48, 0x8D, 0x4C, 0x24, 0x20, 0xE8, 0xD2, 0xCE, 0xF6, 0xFF, 0x84, 0xDB]
+
+
+def test_bipblock04():
+    # color
+    assert BipBlock(0x0180099990).color == 0xffffffff
+    BipBlock(0x0180099990).color = 0xaabbcc
+    assert BipBlock(0x0180099990).color == 0xaabbcc
+    BipBlock(0x0180099990).color = None
+    assert BipBlock(0x0180099990).color == 0xffffffff
+    BipBlock(0x0180099990).color = 0xaabbcc
+    assert BipBlock(0x0180099990).color == 0xaabbcc
+    del BipBlock(0x0180099990).color
+    assert BipBlock(0x0180099990).color == 0xffffffff
+    with pytest.raises(TypeError): BipBlock(0x0180099990).color = "abcd"
+
+
+
+
+
 
 
