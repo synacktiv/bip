b9996a0f96b74dcf2a4cf54a9b965fb249e4e8ea
bruno <bruno>
2020-08-04 18:00:36 +0200

    change Bip version to 0.3 for release

--- bip/__init__.py
+++ bip/__init__.py
@@ -3,6 +3,6 @@ from .gui import *
 from .hexrays import *
 
 VERSION_BIP_MAJOR = 0 #: Major version of Bip
-VERSION_BIP_MINOR = 2 #: Minor version of Bip
+VERSION_BIP_MINOR = 3 #: Minor version of Bip
 
 
